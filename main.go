7d7761328fefe564abf6d2b705394c44cf864d54
Uzzee <kudrinski199iii@gmail.com>
2025-09-11 13:25:55 +0300

    func for converter

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
 	const USDinEUR = 0.84
 	const USDinRUB = 84.92
 	const EURinRUB = USDinRUB / USDinEUR
 }
+
+func getUserInput() (float64, string, string) {
+	var value float64
+	var originalCurrency string
+	var targetCurrency string
+	fmt.Print("Введите исходную валюту: ")
+	fmt.Scan(&originalCurrency)
+	fmt.Print("Введите валюту для конвертации: ")
+	fmt.Scan(&targetCurrency)
+	fmt.Print("Введите колличество: ")
+	fmt.Scan(&value)
+	return value, originalCurrency, targetCurrency
+}
+
+func convert(value float64, originalCurrency string, targetCurrency string) {}
